616d30216b672abf59a1bd915ef65078154cd8a2
jqj0901 <41416733+jqj0901@users.noreply.github.com>
2019-01-17 00:42:29 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -6,7 +6,6 @@ export PATH
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR server
 #	Version: 2.0.38
-#	Author: Huaji
 #=================================================
 
 sh_ver="2.0.38"
